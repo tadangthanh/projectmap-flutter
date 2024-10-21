8d6aae9fa049ee8f0d62a4fddbe432033f1b634c
ta dang thanh <tathanh.mycv@gmail.com>
2024-10-21 13:53:06 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'GoTogether',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
--- lib/map_screen.dart
+++ lib/map_screen.dart
@@ -3,8 +3,6 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:google_maps_flutter_android/google_maps_flutter_android.dart';
-import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
 import 'package:map/bloc/map/map_bloc.dart';
 import 'package:map/bloc/map/map_state.dart';
 
@@ -34,20 +32,20 @@ class _MapScreenState extends State<MapScreen>
   @override
   void initState() {
     super.initState();
-    _initializeMapRenderer();
+    // _initializeMapRenderer();
     _controller = AnimationController(
       vsync: this,
       duration: const Duration(milliseconds: 150),
     );
   }
 
-  void _initializeMapRenderer() {
-    final GoogleMapsFlutterPlatform mapsImplementation =
-        GoogleMapsFlutterPlatform.instance;
-    if (mapsImplementation is GoogleMapsFlutterAndroid) {
-      mapsImplementation.useAndroidViewSurface = true;
-    }
-  }
+  // void _initializeMapRenderer() {
+  //   final GoogleMapsFlutterPlatform mapsImplementation =
+  //       GoogleMapsFlutterPlatform.instance;
+  //   if (mapsImplementation is GoogleMapsFlutterAndroid) {
+  //     mapsImplementation.useAndroidViewSurface = true;
+  //   }
+  // }
 
   bool _isNavigationIcon = false;
 
@@ -69,21 +67,8 @@ class _MapScreenState extends State<MapScreen>
                 onPressed: () {
                   // Navigator.pop(context);
                 },
-
               ),
-              actions: [
-                IconButton(
-                  icon: const Icon(Icons.search),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const SearchScreen(),
-                      ),
-                    );
-                  },
-                ),
-              ],
+              actions: _actions(context),
             ),
             body: Center(
               child: Stack(
@@ -122,72 +107,26 @@ class _MapScreenState extends State<MapScreen>
                           });
                         },
                         child: BackdropFilter(
-                          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                          filter: ImageFilter.blur(sigmaX: 1, sigmaY: 1),
                           // Làm mờ
                           child: Container(
                             color: Colors.black
-                                .withOpacity(0.5), // Nền màu tối với độ mờ
+                                .withOpacity(0.2), // Nền màu tối với độ mờ
                           ),
                         ),
                       ),
                     ),
                   // Nút mở panel
                   Positioned(
-                    top: 16,
-                    right: 16,
-                    child: Container(
-                      width: 40,
-                      decoration: const BoxDecoration(
-                        color: Colors.blue,
-                        shape: BoxShape.circle,
-                      ),
-                      child: FloatingActionButton(
-                        heroTag: 'panel',
-                          shape: const CircleBorder(),
-                          onPressed: _togglePanel,
-                          child: Image.asset(
-                            'assets/icons/stack.png',
-                            color: Colors.black, // Đặt màu icon là đen
-                            width: 25, // Điều chỉnh chiều rộng
-                            height: 25, // Điều chỉnh chiều cao
-                            fit: BoxFit
-                                .contain, // Đảm bảo ảnh được chứa trong widget
-                          )),
-                    ),
+                    top: 26,
+                    right: 26,
+                    child: _typeMapButton(),
                   ),
                   // icon định vị vị trí hiện tại
                   Positioned(
                     bottom: 100,
                     right: 25,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        FloatingActionButton(
-                          heroTag: 'location',
-                          backgroundColor: Colors.white,
-                          shape: const CircleBorder(),
-                          onPressed: () {
-                            _isNavigationIcon = !_isNavigationIcon;
-                            BlocProvider.of<MapBloc>(context).add(
-                                CurrentLocationEvent()); // Cập nhật lại vị trí hiện tại khi bấm nút
-                          },
-                          child: !_isNavigationIcon
-                              ? Image.asset(
-                                  'assets/icons/navigation.png',
-                                  color: Colors.blue,
-                                  width: 20,
-                                  height: 20,
-                                )
-                              : Image.asset(
-                                  'assets/icons/compass.png',
-                                  color: Colors.blue,
-                                  width: 30,
-                                  height: 30,
-                                ),
-                        ),
-                        const SizedBox(height: 10),
-                      ],
-                    ),
+                    child: _floatingActionButtons(context),
                   ),
                   AnimatedPositioned(
                     duration: const Duration(milliseconds: 150),
@@ -196,113 +135,194 @@ class _MapScreenState extends State<MapScreen>
                     // Chiều cao panel
                     left: 0,
                     right: 0,
-                    child: Container(
-                      height: 400, // Chiều cao của panel
-                      color: Colors.white,
-                      padding: const EdgeInsets.only(bottom: 16),
-                      child: Column(
-                        children: [
-                          Container(
-                            padding: const EdgeInsets.all(8),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Container(
-                                  margin: const EdgeInsets.only(left: 8),
-                                  child: const Text("Loại bản đồ"),
-                                ),
-                                IconButton(
-                                  icon: const Icon(Icons.close_outlined),
-                                  onPressed: _togglePanel,
-                                ),
-                              ],
-                            ),
-                          ),
-                          Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Row(
-                                children: [
-                                  Expanded(
-                                      child: _typeMap(
-                                          'Mặc định',
-                                          'assets/icons/icon-map-normal.png',
-                                          MapType.normal,
-                                          state,
-                                          context, () {
-                                    BlocProvider.of<MapBloc>(context).add(
-                                        ChangeMapTypeEvent(MapType.normal));
-                                  })),
-                                  Expanded(
-                                      child: _typeMap(
-                                          'Vệ tinh',
-                                          'assets/icons/icon-map-satellite.png',
-                                          MapType.hybrid,
-                                          state,
-                                          context, () {
-                                    BlocProvider.of<MapBloc>(context).add(
-                                        ChangeMapTypeEvent(MapType.hybrid));
-                                  })),
-                                  Expanded(
-                                      child: _typeMap(
-                                          'Địa hình',
-                                          'assets/icons/icon-map-terrain.png',
-                                          MapType.terrain,
-                                          state,
-                                          context, () {
-                                    BlocProvider.of<MapBloc>(context).add(
-                                        ChangeMapTypeEvent(MapType.terrain));
-                                  })),
-                                ],
-                              )
-                            ],
-                          ),
-                          Container(
-                            height: 0.5, // Độ dày của đường
-                            color: Colors.grey, // Màu sắc của đường
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(8),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Container(
-                                  margin: const EdgeInsets.only(left: 8),
-                                  child: const Text("Chi tiết bản đồ"),
-                                )
-                              ],
-                            ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              SizedBox(
-                                width: MediaQuery.of(context).size.width / 3,
-                                child: _mapDetailElement(
-                                    'Giao thông',
-                                    'assets/icons/icon-map-traffic.png',
-                                    state.trafficEnabled,
-                                    state,
-                                    context, () {
-                                  BlocProvider.of<MapBloc>(context).add(
-                                      ChangeMapDetailEvent(
-                                          !state.trafficEnabled));
-                                }),
-                              )
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
+                    child: _mapMenu(context, state),
                   )
                 ],
               ),
             ),
           );
         }
-        return const Text("ASdasds");
+        return const Text("Error");
       }),
     );
+  }
+
+  Widget _mapMenu(context, state) {
+    return Container(
+      height: 400, // Chiều cao của panel
+      color: Colors.white,
+      padding: const EdgeInsets.only(bottom: 16),
+      child: Column(
+        children: [
+          //header and close button
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(left: 8),
+                  child: const Text("Loại bản đồ"),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.close_outlined),
+                  onPressed: _togglePanel,
+                ),
+              ],
+            ),
+          ),
+          // Danh sach cac loai ban do
+          _listMapType(context, state),
+          Container(
+            height: 0.5, // Độ dày của đường
+            color: Colors.grey, // Màu sắc của đường
+          ),
+          //header
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(left: 8),
+                  child: const Text("Chi tiết bản đồ"),
+                )
+              ],
+            ),
+          ),
+          _listMapDetail(context, state),
+        ],
+      ),
+    );
+  }
+
+  Widget _listMapDetail(context, state) {
+    return Wrap(
+      spacing: 8.0, // Khoảng cách ngang giữa các phần tử
+      runSpacing: 8.0, // Khoảng cách dọc giữa các dòng
+      children: [
+        SizedBox(
+          width: MediaQuery.of(context).size.width / 3 -
+              16, // Điều chỉnh kích thước phần tử
+          child: _mapDetailElement(
+              'Giao thông',
+              'assets/icons/icon-map-traffic.png',
+              state.trafficEnabled,
+              state,
+              context, () {
+            BlocProvider.of<MapBloc>(context)
+                .add(ChangeMapDetailEvent(!state.trafficEnabled));
+          }),
+        ),
+      ],
+    );
+  }
+
+  Widget _listMapType(context, state) {
+    return Wrap(
+      spacing: 8.0, // Khoảng cách ngang giữa các phần tử
+      runSpacing: 8.0, // Khoảng cách dọc giữa các dòng
+      children: [
+        SizedBox(
+          width: MediaQuery.of(context).size.width / 3 -
+              16, // Chia mỗi phần tử thành 1/3 màn hình
+          child: _typeMap('Mặc định', 'assets/icons/icon-map-normal.png',
+              MapType.normal, state, context, () {
+            BlocProvider.of<MapBloc>(context)
+                .add(ChangeMapTypeEvent(MapType.normal));
+          }),
+        ),
+        SizedBox(
+          width: MediaQuery.of(context).size.width / 3 -
+              16, // Đảm bảo kích thước cho mỗi phần tử
+          child: _typeMap('Vệ tinh', 'assets/icons/icon-map-satellite.png',
+              MapType.hybrid, state, context, () {
+            BlocProvider.of<MapBloc>(context)
+                .add(ChangeMapTypeEvent(MapType.hybrid));
+          }),
+        ),
+        SizedBox(
+          width: MediaQuery.of(context).size.width / 3 -
+              16, // Đảm bảo kích thước cho mỗi phần tử
+          child: _typeMap('Địa hình', 'assets/icons/icon-map-terrain.png',
+              MapType.terrain, state, context, () {
+            BlocProvider.of<MapBloc>(context)
+                .add(ChangeMapTypeEvent(MapType.terrain));
+          }),
+        ),
+        // Thêm các phần tử khác nếu cần
+      ],
+    );
+  }
+
+  Widget _floatingActionButtons(context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        FloatingActionButton(
+          heroTag: 'location',
+          backgroundColor: Colors.white,
+          shape: const CircleBorder(),
+          onPressed: () {
+            _isNavigationIcon = !_isNavigationIcon;
+            BlocProvider.of<MapBloc>(context).add(
+                CurrentLocationEvent()); // Cập nhật lại vị trí hiện tại khi bấm nút
+          },
+          child: !_isNavigationIcon
+              ? Image.asset(
+                  'assets/icons/navigation.png',
+                  color: Colors.blue,
+                  width: 20,
+                  height: 20,
+                )
+              : Image.asset(
+                  'assets/icons/compass.png',
+                  color: Colors.blue,
+                  width: 30,
+                  height: 30,
+                ),
+        ),
+        const SizedBox(height: 10),
+      ],
+    );
+  }
+
+  Widget _typeMapButton() {
+    return Container(
+      width: 50,
+      decoration: const BoxDecoration(
+        color: Colors.blue,
+        shape: BoxShape.circle,
+      ),
+      child: FloatingActionButton(
+          tooltip: "Loại bản đồ",
+          heroTag: 'panel',
+          shape: const CircleBorder(),
+          onPressed: _togglePanel,
+          child: Image.asset(
+            'assets/icons/type-map-button.png',
+            color: Colors.black, // Đặt màu icon là đen
+            width: 25, // Điều chỉnh chiều rộng
+            height: 25, // Điều chỉnh chiều cao
+            fit: BoxFit.contain, // Đảm bảo ảnh được chứa trong widget
+          )),
+    );
+  }
+
+  List<Widget> _actions(context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.search),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const SearchScreen(),
+            ),
+          );
+        },
+      ),
+    ];
   }
 
   Widget _typeMap(title, String assetIconUrl, mapType, state, context,
@@ -324,8 +344,8 @@ class _MapScreenState extends State<MapScreen>
             ),
             child: Image.asset(
               assetIconUrl, // Đường dẫn đến hình ảnh bản đồ
-              height: 40, // Chiều cao của hình ảnh
-              width: 40, // Chiều rộng của hình ảnh
+              height: 45, // Chiều cao của hình ảnh
+              width: 45, // Chiều rộng của hình ảnh
             ),
           ),
           const SizedBox(height: 8), // Khoảng cách giữa hình ảnh và văn bản
@@ -333,6 +353,7 @@ class _MapScreenState extends State<MapScreen>
           Text(
             title,
             style: TextStyle(
+                fontSize: 12,
                 color: mapType == state.currentMapType ? Colors.blue : null),
           ),
         ],
@@ -363,15 +384,16 @@ class _MapScreenState extends State<MapScreen>
             ),
             child: Image.asset(
               assetIconUrl, // Đường dẫn đến hình ảnh bản đồ
-              height: 40, // Chiều cao của hình ảnh
-              width: 40, // Chiều rộng của hình ảnh
+              height: 45, // Chiều cao của hình ảnh
+              width: 45, // Chiều rộng của hình ảnh
             ),
           ),
           const SizedBox(height: 8), // Khoảng cách giữa hình ảnh và văn bản
           // Văn bản mô tả
           Text(
             title,
-            style: TextStyle(color: isEnabled == true ? Colors.blue : null),
+            style: TextStyle(
+                fontSize: 12, color: isEnabled == true ? Colors.blue : null),
           ),
         ],
       ),
